3064a06d68a677f1dcd0e33f329dfdf9acc00954
moonlight0905 <harvest.moon.0905@gmail.com>
2023-02-27 14:16:28 +0900

    search,new,createアクション定義

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,27 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.includes(:user).order("created_at DESC")
   end
+
+  def search
+    @recipes = Recipe.search(params[:keyword])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:recipe_name,:image,:category_id,:servings_id,:ingredients1,:ingredients2,:ingredients3,:ingredients4,:ingredients5,:ingredients6,:cooking_instructions1,:cooking_instructions2,:cooking_instructions3,:cooking_instructions4,:cooking_instructions5,:cooking_instructions6,:remarks).merge(user_id: current_user.id)
+  end
+  
 end
