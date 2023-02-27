a1ab22502367edb41e68aa88402e5a458229614d
moonlight0905 <harvest.moon.0905@gmail.com>
2023-02-27 14:17:22 +0900

    検索アクション定義

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,10 @@
 class Recipe < ApplicationRecord
+    def self.search(search)
+      if search != ""
+        search = "%#{search}%"
+        Tweet.find_by_sql(["select * from tweets where text like ? ", search])
+      else
+        Tweet.all
+      end
+    end
 end
