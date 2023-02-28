fbf577770fcac02ae19f069e5e3c5682867412be
moonlight0905 <harvest.moon.0905@gmail.com>
2023-02-28 14:35:51 +0900

    ActiveStorage,ActiveHash導入

---
+++ app/models/category_id.rb
@@ -0,0 +1,11 @@
+class CategoryId < ActiveHash::Base
+  self.data = [
+    { id: 1, name: 'メインのおかず' },
+    { id: 2, name: 'サブのおかず' },
+    { id: 3, name: 'ご飯もの' },
+    { id: 4, name: '麺類' },
+    { id: 5, name: 'お菓子' },
+    { id: 6, name: 'ソース・スープ類' },
+    { id: 7, name: 'その他' }
+  ]
+  end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,7 @@
 class Recipe < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
     def self.search(search)
       if search != ""
         search = "%#{search}%"
