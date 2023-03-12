b314f1d005f5b83e7e689b6632467bb16116ee98
moonlight0905 <harvest.moon.0905@gmail.com>
2023-03-12 21:52:53 +0900

    Recipeモデルの検索機能（クラスメソッド）編集

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -25,9 +25,9 @@ class Recipe < ApplicationRecord
     def self.search(search)
       if search != ""
         search = "%#{search}%"
-        Tweet.find_by_sql(["select * from tweets where text like ? ", search])
+        Recipe.find_by_sql(["select * from recipes where text like ? ", search])
       else
-        Tweet.all
+        Recipe.all
       end
     end
 
